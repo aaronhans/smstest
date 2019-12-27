142543399dc0137658987a943128a0d64ba765c6
Aaron Hans <aaron.hans@alpha.ca.gov>
2019-12-27 12:09:26 -0800

    change body ref

--- src/http/post-sms/index.js
+++ src/http/post-sms/index.js
@@ -1,10 +1,14 @@
+let arc = require('@architect/functions')
+
 exports.handler = async function http(req) {
+  let reqbody = arc.http.helpers.bodyParser(request)
+  let textVal = reqbody.Body
 
   let body = '';
   let twilioResponse = await new Promise(resolve => {
     const MessagingResponse = require('twilio').twiml.MessagingResponse;
     const twiml = new MessagingResponse();
-    twiml.message(`Hello, thanks for contacting Alpha.CA.gov! The nearest food bank to ${req.body} is the Sacramento County food bank at 3333 3rd Ave., Sacramento. Their website is http://www.foodlink.org/ you can reach them via phone at (916) 456-1980, directions: https://maps.google.com/maps?daddr=38.55248,-121.47032`);
+    twiml.message(`Hello, thanks for contacting Alpha.CA.gov! The nearest food bank to ${textVal} is the Sacramento County food bank at 3333 3rd Ave., Sacramento. Their website is http://www.foodlink.org/ you can reach them via phone at (916) 456-1980, directions: https://maps.google.com/maps?daddr=38.55248,-121.47032`);
     body = twiml.toString();
     resolve(twiml.toString())
   });
