737f4d1efb8552bd688efc2024382dd13dbf4771
Aaron Hans <aaron.hans@alpha.ca.gov>
2019-12-26 16:40:28 -0800

    use env vars

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,36 +1,17 @@
-// Add simple, fast, scalable persistence: https://docs.begin.com/en/data/begin-data/
-// let data = require('@begin/data')
+const client = require('twilio')(env.TWILIOSID, env.TWILIOTOKEN);
 
-// Add secure sessions, middleware, and more: https://docs.begin.com/en/functions/http/
-// let arc = require('@architect/functions')
-
-// TODO: modify the body object!
-let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello Aaron!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>
-`
+let body = `hi`
 
 exports.handler = async function http(req) {
-  return {
+  client.messages
+    .create({
+       body: `This is the ship that made the Kessel Run in ${(Math.random() * 100).toFixed(1)} parsecs?`,
+       from: '+14156304521',
+       to: '+19259899425'
+     })
+    .then(message => console.log(message.sid));
+    
+    return {
     headers: {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
@@ -40,16 +21,6 @@ exports.handler = async function http(req) {
 }
 
 // Example responses
-
-/* Forward requester to a new path
-exports.handler = async function http (req) {
-  return {
-    statusCode: 302,
-    headers: {'location': '/about'}
-  }
-}
-*/
-
 /* Respond with successful resource creation, CORS enabled
 let arc = require('@architect/functions')
 exports.handler = arc.http.async (http)
@@ -59,15 +30,6 @@ async function http (req) {
     headers: {'content-type': 'application/json; charset=utf8'},
     body: JSON.stringify({ok: true}),
     cors: true,
-  }
-}
-*/
-
-/* Deliver client-side JS
-exports.handler = async function http (req) {
-  return {
-    headers: {'content-type': 'text/javascript; charset=utf8'},
-    body: 'console.log("Hello world!")',
   }
 }
 */
