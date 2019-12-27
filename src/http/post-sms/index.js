4aba1efb768555a1386b3bdb09d0e55db1d035d5
Begin <support@begin.com>
2019-12-27 11:53:09 -0800

    _ci: adds src/http/post-sms/index.js

--- src/http/post-sms/index.js
+++ src/http/post-sms/index.js
@@ -1,27 +1,12 @@
-exports.handler = async function http (req) {
+// Enable secure sessions, express-style middleware, and more:
+// https://docs.begin.com/en/functions/http/
+//
+// let begin = require('@architect/functions')
 
-  let body = '';
-  let twilioResponse = await new Promise(resolve => {
-    const MessagingResponse = require('twilio').twiml.MessagingResponse;
-    const twiml = new MessagingResponse();
-    twiml.message('The Robots are coming! Head for the hills!');
-    body = twiml.toString();
-    resolve(twiml.toString())
-  });
-
-  console.log('response is '+twilioResponse)
-
-  body = twilioResponse;
-
+exports.handler = async function http(req) {
+  console.log(req)
   return {
-    headers: {
-      'content-type': 'text/xml; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body
+    status: 302,
+    location: '/'
   }
 }
-
-
-
-
