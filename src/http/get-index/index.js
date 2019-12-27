e0cc7a511be40018cb63cccbf78c29b632fb844e
Aaron Hans <aaron.hans@alpha.ca.gov>
2019-12-26 17:02:09 -0800

    reconfigure flow

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -4,20 +4,22 @@ let body = `hi`
 
 exports.handler = async function http(req) {
   client.messages
-    .create({
-       body: `This is the ship that made the Kessel Run in ${(Math.random() * 100).toFixed(1)} parsecs?`,
-       from: '+14156304521',
-       to: '+19259899425'
-     })
-    .then(message => console.log(message.sid));
-    
+  .create({
+      body: `This is the ship that made the Kessel Run in ${(Math.random() * 100).toFixed(1)} parsecs?`,
+      from: '+14156304521',
+      to: '+19259899425'
+    })
+  .then(message => {
+    console.log(message.sid)
     return {
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body
-  }
+      headers: {
+        'content-type': 'text/html; charset=utf8',
+        'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      },
+      body
+    }
+  });
+    
 }
 
 // Example responses
