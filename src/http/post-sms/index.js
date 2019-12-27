2657a32482f422e131f0ce5332da9df735483837
Aaron Hans <aaron.hans@alpha.ca.gov>
2019-12-27 11:55:29 -0800

    oh you create the routes online now

--- src/http/post-sms/index.js
+++ src/http/post-sms/index.js
@@ -1,12 +1,27 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
-
 exports.handler = async function http(req) {
-  console.log(req)
+
+  let body = '';
+  let twilioResponse = await new Promise(resolve => {
+    const MessagingResponse = require('twilio').twiml.MessagingResponse;
+    const twiml = new MessagingResponse();
+    twiml.message('The Robots are coming! Head for the hills!');
+    body = twiml.toString();
+    resolve(twiml.toString())
+  });
+
+  console.log('response is '+twilioResponse)
+
+  body = twilioResponse;
+
   return {
-    status: 302,
-    location: '/'
+    headers: {
+      'content-type': 'text/xml; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body
   }
 }
+
+
+
+
