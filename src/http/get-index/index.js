3584aff6ca398cdf9f5601bd50de2efb1db1b376
Aaron Hans <aaron.hans@alpha.ca.gov>
2019-12-26 17:12:04 -0800

    await twilio promise before proceeding

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,25 +1,33 @@
-const client = require('twilio')(process.env.TWILIOSID, process.env.TWILIOTOKEN);
 
 let body = `hi`
 
 exports.handler = async function http(req) {
-  client.messages
-  .create({
-      body: `This is the ship that made the Kessel Run in ${(Math.random() * 100).toFixed(1)} parsecs?`,
-      from: '+14156304521',
-      to: '+19259899425'
-    })
-  .then(message => {
-    console.log(message.sid)
-    return {
-      headers: {
-        'content-type': 'text/html; charset=utf8',
-        'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-      },
-      body
-    }
+  const client = require('twilio')(process.env.TWILIOSID, process.env.TWILIOTOKEN);
+  console.log('attempting to connect to twilio')
+
+  let twilioResponse = await new Promise(resolve => {
+    client.messages
+    .create({
+        body: `This is the ship that made the Kessel Run in ${(Math.random() * 100).toFixed(1)} parsecs?`,
+        from: '+14156304521',
+        to: '+19259899425'
+      })
+    .then(message => {
+      console.log('connected to twilio')
+      console.log(message.sid)
+      resolve('completed')
+    });        
   });
-    
+
+  console.log('response is '+twilioResponse)
+
+  return {
+    headers: {
+      'content-type': 'text/html; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body
+  }
 }
 
 // Example responses
